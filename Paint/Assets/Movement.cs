5f870d1c57e3793e370b17092943b236babf4ff9
arh4uwe <55301499+arh4uwe@users.noreply.github.com>
2019-09-13 21:05:36 -0400

    Movement
    I added a dot that moves left when A or the left arrow is pressed and right when D or the right arrow is pressed.

---
+++ Paint/Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+        else if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+    }
+}

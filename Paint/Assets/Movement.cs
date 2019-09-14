d14664751bc8e9ffcac090399ee96208e336b605
arh4uwe <55301499+arh4uwe@users.noreply.github.com>
2019-09-13 22:27:56 -0400

    Push the ladder
    You can now push the ladder by pressing Shift. Also, you run faster and fall faster still.

--- Paint/Assets/Movement.cs
+++ Paint/Assets/Movement.cs
@@ -6,6 +6,8 @@ public class Movement : MonoBehaviour
 {
     const int GROUND_LEVEL_Y = 1;
 
+    const int RUN_SPEED = 3;
+
     bool onLadder = false;
 
     // Start is called before the first frame update
@@ -19,27 +21,27 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            transform.Translate(Vector2.left * Time.deltaTime);
+            transform.Translate(Vector2.left * Time.deltaTime * RUN_SPEED);
         }
         else if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            transform.Translate(Vector2.right * Time.deltaTime);
+            transform.Translate(Vector2.right * Time.deltaTime * RUN_SPEED);
         }
         else if (onLadder)
         {
             if (Input.GetKey("w") || Input.GetKey("up"))
             {
-                transform.Translate(Vector2.up * Time.deltaTime);
+                transform.Translate(Vector2.up * Time.deltaTime * RUN_SPEED);
             }
             else if ((Input.GetKey("s") || Input.GetKey("down")) && transform.position.y > GROUND_LEVEL_Y)
             {
-                transform.Translate(Vector2.down * Time.deltaTime);
+                transform.Translate(Vector2.down * Time.deltaTime * RUN_SPEED);
             }
         }
 
         if (!onLadder && transform.position.y > GROUND_LEVEL_Y)
         {
-            transform.Translate(Vector2.down * Time.deltaTime * 3);
+            transform.Translate(Vector2.down * Time.deltaTime * 5);
         }
     }
 
---
+++ Paint/Assets/TriggerCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCollision : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D collider = gameObject.GetComponent<Collider2D>();
+
+        if (Input.GetKey("left shift") && player.transform.position.y <= 1)
+        {
+            // Make ladder susceptible to pushing
+            print("On ground and pushing");
+            collider.isTrigger = false;
+        } else
+        {
+            collider.isTrigger = true;
+        }
+    }
+}
